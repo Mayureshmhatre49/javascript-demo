f44b5557cf2c91a644c32c08d576c308a20a023d
Chaitanya Mhatre <cmh24292@gmail.com>
2020-12-08 11:48:39 +0530

    Added Toggle Functionality & onload Functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+// toggle function
+
+function toggleElement(){
+ var checkBox = document.getElementById("toggleId");
+ var mapData = '<iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=18.786881%2C%2072.885363&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';
+ var imgData ='<img src="img/temple.jpg" style="width:500px;height:600px;" />';
+ var mapDiv = document.getElementById("mapDiv");
+ var imgDiv = document.getElementById("imgDiv");
+ 
+ if (checkBox.checked == true){
+	 imgDiv.innerHTML="";
+	 mapDiv.innerHTML = mapData; 
+ }else{
+	 mapDiv.innerHTML ="";
+	 imgDiv.innerHTML = imgData;
+ }
+ 
+}
+
+function imgLoad(){
+ var imgData ='<img src="img/temple.jpg" style="width:500px;height:600px;" />';
+ var imgDiv = document.getElementById("imgDiv");
+	 imgDiv.innerHTML = imgData;
+}
